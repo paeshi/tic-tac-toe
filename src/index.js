dae72d87d8675c885fde1d6b20ed65fad68c4bf0
stevepae <stevepaemusic@gmail.com>
2021-05-27 22:25:42 -0400

    working game of tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -13,19 +13,32 @@ const Square = (props) => {
 const Board = () => {
   const initialSquares = Array(9).fill(null);
   const [squares, setSquares] = useState(initialSquares);
+  const [xIsNext, setXIsNext] = useState(true);
 
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClickEvent={() => handleClick(i)} />;
   };
+  const winner = calculateWinner(squares);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
 
   const handleClick = (i) => {
     const newSquares = [...squares];
-    newSquares[i] = "X";
+
+    const winnerDeclared = Boolean(calculateWinner(newSquares));
+    const squareFilled = Boolean(newSquares[i]);
+    if (winnerDeclared || squareFilled) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
     setSquares(newSquares);
+    setXIsNext(!xIsNext);
   };
   return (
-    <div style={{ backgroundColor: "skyblue", margin: 10, padding: 20 }}>
-      Board
+    <div className="board">
+      <div className="status">{status}</div>
+
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -47,10 +60,30 @@ const Board = () => {
 const Game = () => {
   return (
     <div className="game">
-      Game
+      <h1 className="title">Tic-Tac-Toe</h1>
       <Board />
     </div>
   );
 };
 
 ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let line of lines) {
+    const [a, b, c] = line;
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
